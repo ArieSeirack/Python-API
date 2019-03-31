1eeef104b82254a0d18ec631e1990a6dfd08e989
ArieSeirack <48173105+ArieSeirack@users.noreply.github.com>
2019-03-31 11:57:05 +0800

    Update to the final version of main code
    删除了一些测试时候展示结果的代码，调换了一些变量和函数的顺序，增添了注释

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import requests
 import json
-import time
 import csv
-from PyQt5.QtWidgets import QApplication,QMainWindow, QWidget, QFileDialog
-
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from interface import Ui_MainWindow
+
 
 # 链接url
 def get_to_link():
@@ -18,15 +18,14 @@ def get_to_link():
         print("链接错误！！！")
         return ''
 
-    # json化字符串
 
-
+# json化字符串
 def json_text(text):
     jd = json.loads(text)
     return jd
 
 
-# 返回实时日期
+# 返回实时日期，处理各个数据
 def date_time(jd):
     ja = jd['data']
     date = ja['queryDate']  # 返回日期
@@ -35,7 +34,7 @@ def date_time(jd):
     return date, alltime, money
 
 
-# 返回影片票房
+# 返回影片票房，处理各个数据
 def movie_price(jd):
     jl = jd['data']['list']
     for i, jls in enumerate(jl, 1):
@@ -43,21 +42,10 @@ def movie_price(jd):
 
         try:
             days = jls['releaseInfo'][2]  # 上映时间
-            if jls['releaseInfo'][3]!='天':
+            if jls['releaseInfo'][3] != '天':
                 days += jls['releaseInfo'][3]
         except:
             days = '点映'
-
-
-        '''if jls['releaseInfo'][2] == '场':  # 不知道为什么会是“场”
-            days = '点映'
-        else:
-            if jls['releaseInfo'][3] != '天':
-                num2 = jls['releaseInfo'][3]
-            else:
-                num2=''
-
-            days = jls['releaseInfo'][2]+num2  # 上映时间'''
 
         totalmoney = jls['sumBoxInfo']  # 影片总票房
         mainmoney = jls['boxInfo']  # 综合票房
@@ -69,16 +57,14 @@ def movie_price(jd):
 
         yield i, name, days, totalmoney, mainmoney, moneyrate, shownumber, showrate, people, showpeople
 
-    # 创建文件夹
 
-
+# 创建文件夹
 def makeasocket(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    # 保存到csv中
 
-
+# 保存到csv中
 def save_to_csv(path, date, alltime, money, movie_price):  # 这个保存是append添加，不是rewrite的添加方法
     with open(path + '猫眼电影专业版实时数据.csv', 'a') as f:
         writer = csv.writer(f)
@@ -91,34 +77,14 @@ def save_to_csv(path, date, alltime, money, movie_price):  # 这个保存是appe
 # 10个格子分别代表对应位置的数据
 
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-
-
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
 
 
-
-
-def main():
-    path = 'D:/数据/猫眼电影专业版数据/'
-    makeasocket(path)  # 创建文件夹
-    text = get_to_link()
-    jd = json_text(text)
-    app = QApplication(sys.argv)
-    myWin = MyMainWindow()
-
-    date, alltime, money = date_time(jd)
-
-    # 显示主数据栏
-    myWin.DateShow.setText(date)
-    myWin.TimeShow.setText(alltime)
-    myWin.SumShow.setText(money)
-
+def main():  # 程序运行的主函数
+    # 用来暂存影片信息的一些list
     ranklist = []
     rlist = ''
     namelist = []
@@ -130,22 +96,42 @@ def main():
     totalboxlist = []
     tblist = ''
 
-    print('***' * 46)
-    print('{:>10s}:{}{:>10s}:{}{:>10s}：{}'.format('日期', date, '时间', alltime, '总票房', money))
-    print('---' * 46)
-    print('{:^6s}{:^20s}{:^10s}{:^12s}{:^12s}{:^10s}{:^10s}{:^6s}{:^6s}{:^6s}'.format('排名', '影片名', '上映时间(/天)',
-                                                                                      '影片总票房(/亿)', '综合票房(/万)',
-                                                                                      '票房占比(%)', '排片场次', '排片占比(%)',
-                                                                                      '场均人次', '上座率(%)'))
-    print('---' * 46)
-    for movie in movie_price(jd):
-        print('{:^6d}{:^20s}{:^20s}{:^20s}{:^12s}{:^11s}{:^13s}{:^10s}{:^10s}{:^10s}'.format(movie[0], movie[1],
-                                                                                             movie[2], movie[3],
-                                                                                             movie[4], movie[5],
-                                                                                             movie[6], movie[7],
-                                                                                             movie[8], movie[9]))
+    text = get_to_link()  # 获取数据
+    jd = json_text(text)  # json化
+    app = QApplication(sys.argv)
+    myWin = MyMainWindow()
 
-        print('---' * 46)
+    date, alltime, money = date_time(jd)  # 获取当前的日期和时间以及当日总票房
+
+    # 两个自定义的槽函数
+    def savetofile():  # 按钮点击后将实时数据保存到文件
+        path = 'D:/数据/猫眼电影专业版数据/'
+        makeasocket(path)  # 创建文件夹
+        save_to_csv(path, date, alltime, money, movie_price(jd))
+        QMessageBox.information(myWin, "保存提示", "保存成功！",
+                                        QMessageBox.Yes , QMessageBox.Yes)
+
+    def showdetails():  # 按钮点击后显示出所选影片的具体信息
+        name = myWin.comboBox_ChooseName.currentText()
+        for n in movie_price(jd):
+            if n[1] == name:
+                myWin.BoxRate.setText(n[5])
+                myWin.ShowNumber.setText(n[6])
+                myWin.ShowRate.setText(n[7])
+                myWin.avgPeople.setText(n[8])
+                myWin.ShowPeople.setText(n[9])
+        myWin.MovieName.setText(name)
+
+    # 显示主数据栏
+    myWin.DateShow.setText(date)
+    myWin.TimeShow.setText(alltime)
+    myWin.SumShow.setText(money)
+
+    # 两个信号和两个槽的连接
+    myWin.ChooseConfirmed.clicked.connect(showdetails)
+    myWin.FileSave.clicked.connect(savetofile)
+
+    for movie in movie_price(jd):  # 从json化的数据中获取各个影片的主要信息
         myWin.comboBox_ChooseName.addItem(movie[1])
         ranklist.append(str(movie[0]))
         namelist.append(movie[1])
@@ -153,6 +139,7 @@ def main():
         totalboxlist.append(movie[3])
         cursumlist.append(movie[4])
 
+    # 分别获取各影片的各个信息
     for rank in ranklist:
         rlist += rank
         rlist += '\n'
@@ -173,40 +160,16 @@ def main():
         tblist += tb
         tblist += '\n'
 
+    # 显示各电影的主要信息
     myWin.RankList.setText(rlist)
     myWin.NameList.setText(nlist)
     myWin.ShowDaysList.setText(sdlist)
     myWin.CurSumList.setText(cslist)
     myWin.TotalBoxList.setText(tblist)
 
-
-
-    def showdetails():
-        name = myWin.comboBox_ChooseName.currentText()
-        for n in movie_price(jd):
-            if n[1] == name:
-                myWin.BoxRate.setText(n[5])
-                myWin.ShowNumber.setText(n[6])
-                myWin.ShowRate.setText(n[7])
-                myWin.avgPeople.setText(n[8])
-                myWin.ShowPeople.setText(n[9])
-        myWin.MovieName.setText(name)
-
-    myWin.ChooseConfirmed.clicked.connect(showdetails)
-
-    # save_to_csv(path, date, alltime, money, movie_price(jd))
-
-    myWin.show() # 显示窗口
-    sys.exit(app.exec_())  # 进入这个GUI的循环之后就不会出来了直到关闭
-
-
-
+    myWin.show()  # 显示窗口
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
     main()
-    '''app = QApplication(sys.argv)
-    myWin = MyMainWindow()
-    myWin.show()
-    sys.exit(app.exec_())  # 进入这个GUI的循环之后就不会出来了直到关闭'''
-
